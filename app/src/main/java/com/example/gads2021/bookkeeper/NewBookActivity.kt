84b0403ea261322eedf86f6ea02630f1dd461ee7
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-10-26 18:06:19 +0100

    Implement the UI for the activities

---
+++ app/src/main/java/com/example/gads2021/bookkeeper/NewBookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gads2021.bookkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
